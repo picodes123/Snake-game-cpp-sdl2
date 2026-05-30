faec6974ac5399ca6c69ea6dea22d3e054434d01
picodes123 <piyush.verma.dev1@gmail.com>
2026-05-31 01:08:26 +0530

    finishing touch

--- Game.cpp
+++ Game.cpp
@@ -10,19 +10,19 @@ using namespace std;
 
 void Game::run()
 {
-    lastMoveTime = SDL_GetTicks();
-    const int moveDelay = 70;
+    lastMoveTime = SDL_GetTicks(); 
+    const int moveDelay = 70; //how much ms snake update aka snake speed
     score = 0;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) //initialize screen
     {
         cout << "SDL Init Error: " << SDL_GetError() << endl;
         return;
     }
-    window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    window = SDL_CreateWindow("Snake Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN); //800x600 game window
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); //render the screen
 
-    srand(time(NULL));
+    srand(time(NULL));  //for snake food 
     int x = (rand()%80) * 10;
     int y = (rand()%60) * 10;
     food = {x,y};
@@ -31,7 +31,7 @@ void Game::run()
     isRunning = true;
     while (isRunning)
     {
-        Uint32 currentTime = SDL_GetTicks();
+        Uint32 currentTime = SDL_GetTicks(); 
 
         if (currentTime - lastMoveTime > moveDelay){
             snake.update();
@@ -39,7 +39,7 @@ void Game::run()
 
             auto head = snake.getHead();
 
-            if (head.first == food.first && head.second == food.second){
+            if (head.first == food.first && head.second == food.second){  //check if snake eat food, hence increase snake length and generate new food
 
                 snake.grow(5);
                 food = {(rand()%80)* 10, (rand()%60)* 10};
@@ -47,13 +47,13 @@ void Game::run()
 
             } 
 
-            if (snake.checkselfcollision()) { 
+            if (snake.checkselfcollision()) {  //close if self collision
 
             isRunning = false;
 
            }
 
-           if (head.first<0 || head.first>=800 || head.second<0 || head.second >=600)
+           if (head.first<0 || head.first>=800 || head.second<0 || head.second >=600)  //for collision with wall(screen height and width)
            {
              isRunning = false;
            }
@@ -68,7 +68,7 @@ void Game::run()
             if (event.type == SDL_QUIT)
                 isRunning = false;
 
-            if (event.type==SDL_KEYDOWN)
+            if (event.type==SDL_KEYDOWN) //detect keyboard input
             {
                 if(event.key.keysym.sym == SDLK_UP){snake.setDirection(0,-1);}
                 if(event.key.keysym.sym == SDLK_DOWN){snake.setDirection(0,1);}
@@ -78,17 +78,18 @@ void Game::run()
             
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-
+        
+        //draw snake
         snake.render(renderer);
 
         
-        SDL_SetRenderDrawColor(renderer,255,0 ,0 ,255);
+        SDL_SetRenderDrawColor(renderer,255,0 ,0 ,255); //red color for food
 
-        SDL_Rect foodRect = {food.first, food.second, 10, 10};
+        SDL_Rect foodRect = {food.first, food.second, 10, 10}; 
         SDL_RenderFillRect(renderer, &foodRect);
 
         SDL_RenderPresent(renderer);
--- Snake.cpp
+++ Snake.cpp
@@ -1,5 +1,5 @@
 #include "Snake.h"
-Snake::Snake()
+Snake::Snake()  //snake body
 {
     body.push_back({100, 100});
     body.push_back({90, 100});
@@ -10,6 +10,7 @@ Snake::Snake()
 }
 
 
+//for snake update
 void Snake::update(){
 
     for (int i = body.size()-1; i > 0; i--){
@@ -22,6 +23,8 @@ void Snake::update(){
     body[0].second += dirY * CELL_SIZE;
 }
 
+
+//draws the snake body 
 void Snake::render(SDL_Renderer* renderer){
     for (int i = 0; i < body.size(); i++)
     {
@@ -29,17 +32,23 @@ void Snake::render(SDL_Renderer* renderer){
         SDL_RenderFillRect(renderer, &rect);
     } }
 
+
+//how much snake grow after eating food
 void Snake::grow(int amount){
     for(int i =0; i < amount; i++)
     body.push_back(body.back());
 }
 
+
+//for snake head
 std::pair<int,int> Snake::getHead(){
 
     return body[0];
 
 }
 
+
+//to make snake move in direction and condition for it to not move opposite dir
 void Snake::setDirection(int newX,int newY){
 
     if (newX == -dirX && newY == -dirY)
@@ -53,6 +62,8 @@ void Snake::setDirection(int newX,int newY){
 
 }
 
+
+//for checking snakes self collision
 bool Snake::checkselfcollision(){
 
     auto head = body[0];
